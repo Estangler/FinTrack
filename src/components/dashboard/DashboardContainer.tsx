12d6cb26f04be9d7cb0f28c7e9d27928cd78a34d
Estangler <estanglerv@gmail.com>
2026-05-05 13:32:02 -0300

    feat: update DashboardContainer layout and styles for improved responsiveness

--- src/components/dashboard/DashboardContainer.tsx
+++ src/components/dashboard/DashboardContainer.tsx
@@ -4,17 +4,27 @@ import TransactionsContainer from "./TransactionsContainer";
 
 export default function DashboardContainer() {
   return (
-    <div className="p-6 overflow-auto space-y-5 w-full">
-      <header>
-        <h3 className="font-serif text-2xl text-txt-primary">Dashboard</h3>
-        <p className="text-xs mt-0.5 text-txt-muted">
-          Visão geral das suas finanças
-        </p>
+    <div className="p-6 overflow-auto w-full">
+      <header className="flex justify-between items-center mb-5">
+        <div>
+          <h3 className="font-serif text-2xl text-txt-primary">Dashboard</h3>
+          <p className="text-xs mt-0.5 text-txt-muted">
+            Visão geral das suas finanças
+          </p>
+        </div>
+
+        <button
+          className={`px-6 h-10 text-start rounded-xl items-center transition duration-200 ease-linear text-base text-sm cursor-pointer bg-linear-to-r from-accent-gold to-txt-primary shadow-2xl shadow-accent-gold hover:scale-110 hover:shadow-md`}
+        >
+          + Nova
+        </button>
       </header>
 
-      <Card />
-      <TransactionsContainer />
-      <GraficContainer />
+      <main className="flex flex-col gap-4">
+        <Card />
+        <TransactionsContainer />
+        <GraficContainer />
+      </main>
     </div>
   );
 }
